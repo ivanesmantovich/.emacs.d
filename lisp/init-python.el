b13c7ae6293f6f3f1da12360af331f8fba5f8f35
Ivan Esmantovich <waterframe@magic.local>
2023-04-12 18:51:12 +0300

    Python-mode tweak

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,8 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Built-in python-mode, DO NOT use "python-mode" package
+(use-package python
+  :config
+  ;; Undo dumb remap
+  (define-key python-mode-map [remap backward-sentence] nil)
+  (define-key python-mode-map [remap forward-sentence] nil))
+
 (use-package blacken
   :hook (python-mode . blacken-mode))
+
+(use-package pyvenv
+  :config (pyvenv-mode 1))
 
 (provide 'init-python)
 
